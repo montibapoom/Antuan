98eefbd48e12dffd797f05d3b4d4c696ebaa1fad
Bogdan <montibapoom@gmail.com>
2022-07-02 18:06:08 +0300

    Init

---
+++ Antuan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Antuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
